ee0333c9ee9a437d010e2c558cdb68660f7cba7b
Niall Toibin <niall.toibin2@mail.dcu.ie>
2025-03-08 15:48:36 +0000

    Declared setAlarm1, readAlarm1, setAlarm2, readAlarm2 in header file

--- DS3231.h
+++ DS3231.h
@@ -23,6 +23,14 @@ public:
 
    void setTime(uint8_t year, uint8_t month, uint8_t date, uint8_t hour, uint8_t min, uint8_t sec);
 
+   // Sets alarm 1, only for an exact match on time
+   // Will add other modes later if feasible
+   void setAlarm1(uint8_t alarm1sec, uint8_t alarm1min, uint8_t alarm1hour, uint8_t alarm1val, bool dydt = false);
+   void readAlarm1();
+
+   // Set alarm 2.
+   void setAlarm2(uint8_t alarm2min, uint8_t alarm2hour, uint8_t alarm2val, bool dydt = false);
+   void readAlarm2();
 
 };
 
